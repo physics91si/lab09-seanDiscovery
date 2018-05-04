8ae53977b16c94ed7467f1ed7620acdd88eb9860
Sean Mullane <smullane@stanford.edu>
2018-05-03 17:55:57 -0700

    submitting lab 9

--- dynamics.py
+++ dynamics.py
@@ -15,16 +15,35 @@ import particle
 def init_molecule():
     """Create Particles p1 and p2 inside boundaries and return a molecule
     connecting them"""
-
-    pass
-
+    position1 = np.array([0.2, 0.1]) #np.random.rand(2)
+    position2 = np.array([0.8, 0.8]) #np.random.rand(2) 
+    M1 = 1.0
+    M2 = 2.0 
+    k = 1.0 
+    equil_length = 0.5 
+    molecule = particle.Molecule(position1, position2, M1, M2, k, equil_length)
+    return molecule
 
 # TODO: Implement this function
 def time_step(dt, mol):
     """Sets new positions and velocities of the particles attached to mol"""
-    
-    pass
+    #p1 = mol.p1 
+    #p2 = mol.p2 
+    #print(p1.pos, p2.pos) 
 
+    # update velocity 
+    v_1_update = mol.p1.vel + dt*mol.get_force()/mol.p1.m
+    v_2_update = mol.p2.vel - dt*mol.get_force()/mol.p2.m 
+    mol.p1.vel = v_1_update
+    mol.p2.vel = v_2_update
+
+    # update position 
+    position_1_update = mol.p1.pos + mol.p1.vel*dt
+    position_2_update = mol.p2.pos + mol.p2.vel*dt
+    mol.p1.pos = position_1_update 
+    mol.p2.pos = position_2_update
+
+    #print(p1.pos, p2.pos) 
 
 #############################################
 # The rest of the file is already implemented
@@ -46,9 +65,11 @@ def run_dynamics(n, dt, xlim=(0, 1), ylim=(0, 1)):
             fargs=(dt, mol,line), interval=50, blit=False)
     plt.show()
 
-def update_anim(i,dt, mol,line):
+def update_anim(i,dt, mol, line):
     """Update and draw the molecule. Called by FuncAnimation"""
     time_step(dt, mol)
+    print("Molecule 1 ", mol.p1.pos)
+    print("molecule 2 ", mol.p2.pos) 
     line.set_data([(mol.p1.pos[0], mol.p2.pos[0]),
                    (mol.p1.pos[1], mol.p2.pos[1])])
     return line,
--- particle.py
+++ particle.py
@@ -4,13 +4,46 @@
 
 import numpy as np
 
-class Particle:
+class Particle(object):
     """Stores information about a particle with mass, position, and velocity."""
     
     def __init__(self, Position, M):
-        """Create a particle with position (numpy array of len 2) and mass."""
-        self.pos = Position   # Sets x position
+        """Create a particle with Position (numpy array of len 2) and mass."""
+        self.pos = Position   # Set position
         self.m = M  # Sets mass
         # Initial velocity and acceleration set to be zero
-        self.vel = np.zeros((2,1))
-        self.acc = np.zeros((2,1))
+        self.vel = np.zeros((2,))
+        self.acc = np.zeros((2,))
+
+class Molecule(Particle): 
+	""" 
+	Constructs an instance of a Molecule object, using the Particle class defined above. 
+	The function get_displ returns the displacement between the particle 1 and particle 2: 
+		get_displ = p1.pos - p2.pos. 
+	The function get_force returns the force on particle 1 from particle 2, calculated from 
+	Hooke's law. 
+	"""
+
+	def __init__(self, position1, position2, M1, M2, k, equil_length):
+		"""Construct two particles from Particle class, spring constant, and bond equilibrium length """
+		self.p1 = Particle(position1, M1)
+		self.p2 = Particle(position2, M2)
+		self.k = k 
+		self.eq_length = equil_length 
+
+	def get_displ(self):
+		# returns displacement between two particles 
+		return self.p1.pos - self.p2.pos 
+
+	def get_force(self):
+		# obtain angle of displacement vector from displacement function 
+		delta = self.get_displ()
+		theta = np.arctan2(delta[1], delta[0])
+		
+		# project the equilibrium length vector onto x, y axes 
+		equil_vector = np.zeros(2)
+		equil_vector[1] = self.eq_length*np.sin(theta)
+		equil_vector[0] = self.eq_length*np.cos(theta)
+
+		# compute the force 
+		return -self.k*(delta - equil_vector)
